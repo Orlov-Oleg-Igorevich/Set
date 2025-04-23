1b79dc3718aafb58d686d38e0a084976f540b01b
Oleg <oleg_orlov_2005@mail.ru>
2025-04-23 17:58:36 +0300

    fix problem

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import { Team } from './components/gameMechanics/Team';
-import Character, { Bowerman, Swordsman, Magician, Daemon, Undead, Zombie } from './components/gameMechanics/Characters';
-
-let team = new Team;
